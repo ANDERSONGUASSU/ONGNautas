63846b0beb314eb901da7b4284a24a7f84bb55f0
weblerson <zonejr4@gmail.com>
2023-08-16 18:43:36 -0400

    feat: now transparency view has some dynamic values and implemented caching on it

--- apps/ong_admin/models.py
+++ apps/ong_admin/models.py
@@ -6,4 +6,5 @@ class Expenses(models.Model):
     title = models.CharField(max_length=50, blank=False)
     description = description = models.TextField()
     amount_spent = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
-    date = models.DateField(default=datetime.now())
+    date = models.DateField(default=datetime.now, blank=True)
+    
