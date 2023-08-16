b039104b29bdad2399aa93039218ecbf9129d657
diegokropotoff <kropotoff.diego@gmail.com>
2023-08-16 15:36:20 -0300

    model updated

--- apps/ong_admin/models.py
+++ apps/ong_admin/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Expenses(models.Model):
-    title = models.CharField(_("title"), max_length=50, blank=False)
+    title = models.CharField(max_length=50, blank=False)
     description = description = models.TextField(_('description'))
     amount_spent = models.DecimalField(_('amount spent'), max_digits=6, decimal_places=2, blank=False)
