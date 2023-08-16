9af26c2018e668295a114b8919ac3c70593c5a99
Luciana Vivarelli <lucianavivarelli@hotmail.com>
2023-08-16 03:06:01 -0300

    perfil changes

--- apps/perfil/urls.py
+++ apps/perfil/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
      path('', views.index_view, name="index"),
      path('voluntary/', views.voluntary_view, name="voluntary"),
      path('supporter/', views.supporter_view, name="supporter"),
-     
      ]
