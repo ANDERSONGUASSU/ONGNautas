118ba9a86cf8d79142800165f32dd8ec5c3e8da4
AndersonGuassu <andersonguassu@hotmail.com>
2023-08-17 05:43:03 -0300

    arrumando url

--- apps/ong_admin/urls.py
+++ apps/ong_admin/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ong_admin_view, name='ong_admin'),
-    path('/approve/<int:junction_id>', views.confirm_voluntary_participation, name='approve_participation'),
-    path('/update_expense/<int:id>', views.update_expenses_per_project, name='update_project_expense'),
+    path('approve/<int:junction_id>/', views.confirm_voluntary_participation, name='approve_participation'),
+    path('update_expense/<int:id>/', views.update_expenses_per_project, name='update_project_expense'),
 ]
